d54f08499d6d25e4caee66f89b06e849ef5ee391
ramhak <ramil.hakim1@gmail.com>
2017-10-24 14:09:18 +0200

    implemented agg_function

--- Kinematics_side_view.py
+++ Kinematics_side_view.py
@@ -3,6 +3,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+################################################################ FUNCTIONS ######################################################################
+
+def post_agg_dataframe_constructor(ingoing_dataframe_with_multiindex):
+    number_of_indices = len(ingoing_dataframe_with_multiindex.index.names)
+
+    dataframe_new = pd.DataFrame(ingoing_dataframe_with_multiindex.values)
+    dataframe_new.columns = ingoing_dataframe_with_multiindex.columns.levels[1]
+
+    counter = 0
+    while counter < number_of_indices:
+        dataframe_new[ingoing_dataframe_with_multiindex.index.names[counter]] = ingoing_dataframe_with_multiindex.index.get_level_values(level=counter)
+        counter +=1
+
+    return dataframe_new
+
 ################################################################ IMPORTING DATA & CREATING DATA SUBSETS ######################################################################
 #1. Importing data
 DT = pd.read_csv("merge_side_view.csv")
@@ -40,32 +56,28 @@ DT_angles = DT_angles.merge(animal_key, on='RH.index')
 
 ################################################################  AGGREGATING DATA ######################################################################
 
-#A. AGGREGATING ON INDIVIDUAL LEVEL (FOR STATISTICAL ANALYSIS & PLOTTING)
-DT_distance_aggregate_raw = DT_distance.groupby(['RH.index', 'day']).agg({'iliac_crest_height' : {'mean', 'median', 'count'}})
+#1. AGGREGATING ON INDIVIDUAL LEVEL (FOR STATISTICAL ANALYSIS & PLOTTING)
 
-def post_agg_dataframe_constructor(ingoing_dataframe_with_multiindex):
-    number_of_indices = len(ingoing_dataframe_with_multiindex.index.names)
+#A. DISTANCE
 
-    dataframe_new = pd.DataFrame(ingoing_dataframe_with_multiindex.values)
-    dataframe_new.columns = ingoing_dataframe_with_multiindex.columns.levels[1]
+DT_distance_aggregate_raw = DT_distance.groupby(['RH.index', 'day']).agg({'iliac_crest_height' : {'mean'}})
+#Adjusting output with post_agg adjuster function
+DT_distance_aggregate = post_agg_dataframe_constructor(DT_distance_aggregate_raw)
+#Merging aggregate dataset with animal_key
+DT_distance_aggregate.merge(animal_key, on="RH.index")
 
-    counter = 0
-    while counter < number_of_indices:
-        dataframe_new[ingoing_dataframe_with_multiindex.index.names[counter]] = ingoing_dataframe_with_multiindex.index.get_level_values(level=counter)
-        counter +=1
+#B. Angles
 
-    return dataframe_new
-
-post_agg_dataframe_constructor(DT_distance_aggregate_raw)
+DT_angles
 
 
+#2. AGGREGATING ON TREATMENT GROUP LEVEL(FOR SUMMARY & PLOTTING)
 
+DT_distance['iliac_crest_height'].head()
 
 
 
-#B. AGGREGATING ON TREATMENT GROUP LEVEL(FOR SUMMARY & PLOTTING)
-
-
+################################################################  STATISTICAL ANALYSIS ######################################################################
 
 ################################################################  PLOTTING DATA ######################################################################
 
