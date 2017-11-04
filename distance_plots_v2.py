f6d99e345896db98dac6d9c213b8f3b1db2888a8
ramhak <ramil.hakim1@gmail.com>
2017-11-04 21:50:21 +0100

    class defined

--- distance_plots_v2.py
+++ distance_plots_v2.py
@@ -3,53 +3,108 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#1. Importing data
-data_set_raw = pd.read_csv('merge_side_view.csv')
+class DistancePlot:
+
+    def __init__(self, data_frame):
+        self.data_frame = data_frame
+
+    def column_adjuster(self, keep_cols, new_column_names):
+        self.data_frame = self.data_frame[keep_cols]
+        self.data_frame = self.data_frame.rename(columns = new_column_names)
+
+    def key_data_adder(self, key_data):
+        self.data_frame['RH.index'] = self.data_frame['RH.index'].map(lambda rh_index: rh_index[:3])
+        self.data_frame['RH.index'] = self.data_frame['RH.index'].astype('int64')
+        self.data_frame = self.data_frame.merge(key_data, on='RH.index')
+
+    def measure_adjuster(self, adjust_variable, measure='displacement'):
+        self.data_frame['adjustor_column'] = self.data_frame[measure].map(lambda value: value/min(self.data_frame[measure]))
+        self.data_frame.loc[self.data_frame['day']==3, 'adjustor_column'] = 1
+        self.data_frame[adjust_variable+'_adjust'] = self.data_frame[adjust_variable]*self.data_frame['adjustor_column']
+
+    def indexator(self, variable):
+        dictionary = dict(list(self.data_frame.groupby('RH.index')))
+        keys = dictionary.keys()
+
+        def normalizer(dict, dict_key):
+            #divisor = dict[dict_key].loc[dict[dict_key]['day'] == 3, variable].mean()
+            divisor = self.data_frame.min()[variable]
+            dict[dict_key][variable] = dict[dict_key][variable] / divisor
+            return dict[dict_key]
+
+        self.data_frame = pd.concat(list(map(lambda key: normalizer(dictionary, key), keys)), axis=0)
+
+    def column_cleanup(self, keep_columns):
+        self.data_frame = self.data_frame[keep_columns]
+
+    def aggregate_per_animal(self):
+        return self.data_frame.groupby(['RH.index', 'day', 'group'], as_index=False).median()
+
+    def summary(self):
+        return self.data_frame.groupby(['day', 'group'], as_index=False).mean()
+
+#0. Importing key data (RH.index, group, force and displacment)
 animal_key = pd.read_csv('animal_key_kinematics.csv')
 
-#2. Removing unnecessary columns
-keep_columns = ['RH.index', 'day', 'Dist [cm].1', 'Dist [cm].2', 'Dist [cm].3', 'Dist [cm].4', 'Dist [cm].5']
-data_set = data_set_raw[keep_columns]
+#1. Managing ILIAC CREST HEIGHT
+#A. Importing data, creating variables
+data_set_iliac = pd.read_csv('merge_side_view.csv')
 
-#3. Renaming columns
-new_column_names = {'Dist [cm].1':'iliac_crest_height', 'Dist [cm].2':'iliac_crest_2_trochanter_major', 'Dist [cm].3':'trochanter_major_2_knee',
+keep_columns_iliac = ['RH.index', 'day', 'Dist [cm].1', 'Dist [cm].2', 'Dist [cm].3', 'Dist [cm].4', 'Dist [cm].5']
+new_column_names_iliac = {'Dist [cm].1':'iliac_crest_height', 'Dist [cm].2':'iliac_crest_2_trochanter_major', 'Dist [cm].3':'trochanter_major_2_knee',
                                            'Dist [cm].4':'knee_2_ankle', 'Dist [cm].5':'ankle_2_toe'}
+#B. Creating instance, calling methods
+instance_iliac = DistancePlot(data_set_iliac)
+instance_iliac.column_adjuster(keep_columns_iliac, new_column_names_iliac)
+instance_iliac.key_data_adder(animal_key)
+instance_iliac.measure_adjuster('iliac_crest_height')
 
-data_set = data_set.rename(columns = new_column_names)
+instance_iliac.indexator('iliac_crest_height_adjust')
+instance_iliac.column_cleanup(['RH.index', 'day', 'group', 'iliac_crest_height_adjust'])
 
-#4. Merging with animal key
-data_set['RH.index'] = data_set['RH.index'].map(lambda rh_index: rh_index[:3])
-data_set['RH.index'] = data_set['RH.index'].astype('int64')
-data_set = data_set.merge(animal_key, on='RH.index')
 
-#5. Keeping only iliac crest height
-data_set_subset = data_set[['RH.index', 'day', 'group', 'displacement', 'iliac_crest_height']]
+data_aggregate_iliac = instance_iliac.aggregate_per_animal()
+data_summary_iliac = instance_iliac.summary()
 
-#6. Creating displacement index
-data_set_subset = data_set_subset.assign(displacement_index = lambda disp: disp.displacement/min(disp.displacement))
+#2. Managing INTER KNEE DISTANCE
+#A. Importing data, creating variables
+data_set_knee = pd.read_csv('merge_bottom_view.csv')
 
-#7.Adjusting iliac crest height with displacement index
-data_set_subset = data_set_subset.assign(iliac_crest_height_adjust = data_set_subset['iliac_crest_height']*data_set_subset['displacement_index'])
+keep_columns_knee = ['Dist [cm].1', 'RH.index', 'day']
+new_column_names_knee = {'Dist [cm].1': 'inter_knee_distance'}
 
-#8. Aggregating per animal (RH.index)
-data_set_subset_aggregate = data_set_subset.groupby(['RH.index', 'day', 'group'], as_index=False).mean()
+#B. Creating instance, calling methods
+instance_knee = DistancePlot(data_set_knee)
+instance_knee.column_adjuster(keep_columns_knee, new_column_names_knee)
+instance_knee.key_data_adder(animal_key)
+instance_knee.measure_adjuster('inter_knee_distance')
+instance_knee.indexator('inter_knee_distance_adjust')
+instance_knee.column_cleanup(['RH.index', 'day', 'group', 'inter_knee_distance_adjust'])
 
-#9. Aggregating per group
-data_set_subset_summary = data_set_subset.groupby(['day', 'group'], as_index=False).mean()
+data_aggregate_knee = instance_knee.aggregate_per_animal()
+data_summary_knee = instance_knee.summary()
 
-#10. Defining color palette
+#3. Defining color palette for plotting
 palette_BrBG = pd.DataFrame(list(sns.color_palette("BrBG", 7)))
 palette_RdBu_r = pd.DataFrame(list(sns.color_palette("RdBu_r", 7)))
 palette_custom_1 = [tuple(palette_BrBG.iloc[0,:]), tuple(palette_RdBu_r.iloc[0,:]), tuple(palette_RdBu_r.iloc[6,:])]
 
-#11. Plotting
-sns.stripplot(data = data_set_subset, x='day', y='iliac_crest_height_adjust', hue='group', size=5, palette=palette_custom_1,
-              dodge=True, jitter=0.25, alpha=0.5, marker='*')
-sns.despine(left=True, top=True)
-plt.xlabel('Day (Post SCI)', size=12)
-plt.ylabel('Iliac crest height', size=12)
-plt.title('Iliac crest height', size=15, fontweight='bold')
-plt.legend(['SCI', 'SCI+Medium', 'SCI+Medium+IDmBMSCs'], loc='lower center', frameon=False, ncol=3, fontsize=8)
+#4. Plotting
+def over_time_plot(data_set, y_variable, y_label):
+    #sns.stripplot(data = data_set, x='day', y=y_variable, hue='group', size=15, palette=palette_custom_1,
+    #              dodge=True, jitter=0.25, alpha=0.5, marker='o')
+    sns.regplot(data = data_set, x='day', y=y_variable, hue='group')
+    sns.despine(left=True, top=True)
+    plt.xlabel('Day (Post SCI)', size=20, fontweight='bold')
+    plt.ylabel(y_label, size=20, fontweight='bold')
+    plt.legend(['SCI', 'SCI+Medium', 'SCI+Medium+IDmBMSCs'], loc='lower center', frameon=False, ncol=3, fontsize=12)
+
+over_time_plot(instance_iliac.data_frame, 'iliac_crest_height_adjust', 'Iliac crest height index')
+
+#regplot
+    #1 for each group -> raw data
+    #2 for each group -> biological replicates
+    #1 lowess for data
 
 
 
